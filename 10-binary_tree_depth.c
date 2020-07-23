51d276c19a88ccca5d10f03bc438a4d539a681a4
Camilo Tobon <1611@holbertonschool.com>
2020-07-23 13:53:43 -0500

    adding fixes

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	size = binary_tree_depth(tree->parent); 
+	size = binary_tree_depth(tree->parent);
 
 	if (!tree->parent)
 		return (0);
